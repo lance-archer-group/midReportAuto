1c7ad9a37886c2fb7a2ec2a4373f8de759cbf610
lance-archer-group <lance.h@archer.group>
2025-08-21 09:56:02 -0600

    update .sh file

--- trigger.sh
+++ trigger.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-curl -fsS -XPOST -H "x-api-key:${JOB_API_KEY}" "http://127.0.0.1:${JOB_PORT:-3889}/run"
+set -eu
+: "${JOB_PORT:=3889}"
+: "${JOB_API_KEY:?set JOB_API_KEY in the container env}"
+
+curl -fsS --retry 3 --retry-connrefused --max-time 60 \
+  -H "x-api-key:${JOB_API_KEY}" \
+  "http://127.0.0.1:${JOB_PORT}/run"
